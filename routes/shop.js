4835f0c4e0b5785025e4289c6088ec456043cc8f
navyashree16 <navyashreeh162001>
2023-11-21 14:33:57 +0530

    html , css ,js

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,14 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('./util/add');
+
 
 const router = express.Router();
 
 router.get('/',(req, res, next) => {
-    res.send('<h1>Hello from express!</h1>');
+    res.sendFile(path.join(rootDir,  'views', 'shop.html'));
   });
 
 
